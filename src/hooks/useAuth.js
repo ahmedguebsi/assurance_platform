2f4ee2b7d603851d270d8be8b8c511e69f68db7c
Ahmed Guebsi <ahmed.guebsi@enis.tn>
2022-12-08 23:52:28 +0100

    third  commit

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
-
+//generate a token
+//require('crypto').randomBytes(64).toString('hex')
 const useAuth = () => {
     return useContext(AuthContext);
 }
